a55b907367488d9ca6b0f62b960992f9530173ff
Erik Recio <erik.recio@gmail.com>
2023-06-16 21:07:22 +0200

    lets go

---
+++ fourier_coefficients_dD.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pennylane as qml
+from pennylane.fourier import circuit_spectrum, coefficients
+from functools import partial
+
+
+def fourier_coefficients_dD(circuit, w, x):
+
+    d = len(x)
+
+    # Obtain the frequencies of the circuit with this function
+    freqs = circuit_spectrum(circuit)(w, x)
+
+    degree = []
+    pos_freqs = {}
+    for var, fs in freqs.items():
+        num_freqs = (len(freqs[var])-1)//2
+        
+        pos_freqs[var] = fs[num_freqs:] # Keep only the positive frequencies
+        degree.append(num_freqs) # Degree of the fourier series for each variable
+
+    coeffs = coefficients(partial(circuit, w), d, degree) # Calculate the fourier coefficients for the exponential expansion
+    pos_coeffs = sin_cos_transf(d, coeffs, freqs) # Transform the coefficients into the sine-cosine expansion
+
+    return pos_freqs, np.round(pos_coeffs, decimals=4)
+
+
+
+# Recursive function. Finds all the possible combinations of the frequencies
+# given all the features and calculates the sine-cosine coefficients
+def sin_cos_transf(d, coeffs, freqs, pos=[], pos_done=[], pos_coeffs = [], last_i=-1):
+    current_i = last_i + 1
+    if current_i == d:
+        
+        if tuple(pos) not in pos_done:
+            
+            coeff_search = coeffs
+            for k in range(d):
+                coeff_search = coeff_search[pos[k]]
+                
+            c = coeff_search
+
+            if tuple(pos) == tuple([0]*d):
+                cos_coef = c
+                sin_coef = 0
+            else:
+                cos_coef = c + np.conj(c)
+                sin_coef = 1j*(c - np.conj(c))
+            
+            pos_coeffs.extend([np.real(cos_coef), np.real(sin_coef)])
+
+            minus_pos = []
+            for j in pos:
+                minus_pos.append(-j)
+
+            pos_done.extend([tuple(pos), tuple(minus_pos)])
+
+        del pos[-1]
+    else:
+        num_freqs = len(freqs[list(freqs)[current_i]])
+        for f in range(-(num_freqs-1)//2, (num_freqs-1)//2 + 1):
+            pos.append(f)
+            sin_cos_transf(d, coeffs, freqs, pos, pos_done, pos_coeffs, last_i=current_i)
+        try:
+            del pos[-1]
+        except Exception:
+            pass
+
+    # The coefficients returned are in increasing order with respect to the frequencies.
+    return pos_coeffs
+
+
+# Example of the order of returned coefficients
+
+# 'x0': [0, 1, 2]
+# 'x1': [0, 1]
+
+# [-2, -1]
+# [-2,  0]
+# [-2,  1]
+# [-1, -1]
+# [-1,  0]
+# [-1,  1]
+# [ 0, -1]
+# [ 0,  0]
+# [ 0,  1] (doesn't appear because same as [ 0, -1])
+# [ 1, -1] (doesn't appear because same as [-1,  1])
+# [ 1,  0] (doesn't appear because same as [-1,  0])
+# [ 1,  1] (doesn't appear because same as [-1, -1])
+# [ 2, -1] (doesn't appear because same as [-2,  1])
+# [ 2,  0] (doesn't appear because same as [-2,  0])
+# [ 2,  1] (doesn't appear because same as [-2, -1])
--- tests.py
+++ tests.py
@@ -2,6 +2,8 @@ import numpy as np
 import pennylane as qml
 from pennylane.fourier import circuit_spectrum, coefficients
 from functools import partial
+from fourier_coefficients_1D import fourier_coefficients_1D
+from fourier_coefficients_dD import fourier_coefficients_dD
 
 dev = qml.device('default.qubit', wires=2)
 
@@ -24,33 +26,11 @@ def circuit_with_weights(w, x):
 
     qml.RX(x[0], wires=0, id="x0")
     # qml.RY(x[1], wires=1, id="x1")
+    qml.RX(x[2], wires=0, id="x2")
     qml.CNOT(wires=[1, 0])
 
     return qml.expval(qml.PauliZ(0))
 
-
-def fourier_coefficients_1D(circuit, w, x):
-
-    all_freqs = circuit_spectrum(circuit)(w, x)
-    freqs = next(iter(all_freqs.values()))
-
-    num_coeffs = (len(freqs)-1)//2
-    coeffs = coefficients(partial(circuit, w), 1, num_coeffs)
-
-    pos_freqs = freqs[num_coeffs:]
-    pos_coeffs = []
-
-    for i, c in enumerate(coeffs[:num_coeffs+1]):
-        if i == 0:
-            cos_coef = c
-            sin_coef = 0
-        else:
-            cos_coef = c + np.conj(c)
-            sin_coef = 1j*(c - np.conj(c))
-        
-        pos_coeffs.extend([np.real(cos_coef), np.real(sin_coef)])
-
-    return pos_freqs, np.round(pos_coeffs, decimals=4)
 
 
 
@@ -73,37 +53,8 @@ weights = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]])
 
 
 
+freqs1, coeffs1 = fourier_coefficients_1D(simple_circuit_marked, weights, [0.1])
+print(freqs1, coeffs1)
 
-
-
-
-def fourier_coefficients_d_dim(circuit, w, x):
-
-    d = len(x)
-
-    freqs = circuit_spectrum(circuit)(w, x)
-    num_coeffs = []
-    pos_freqs = {}
-
-    for var, fs in freqs.items():
-        num_freqs = (len(freqs[var])-1)//2
-        
-        pos_freqs[var] = fs[num_freqs:]
-        num_coeffs.append(num_freqs)
-
-    coeffs = coefficients(partial(circuit, w), d, num_coeffs)
-    
-
-    
-
-
-
-
-    print(freqs)
-    print(np.round(coeffs, decimals=4))
-    print(np.round(coeffs[0][-1], decimals=4))
-
-    print(pos_freqs)
-
-
-fourier_coefficients_d_dim(circuit_with_weights, weights, [0.3, 0.1])
+freqs, coeffs = fourier_coefficients_dD(simple_circuit_marked, weights, [0.1])
+print(freqs, coeffs)
