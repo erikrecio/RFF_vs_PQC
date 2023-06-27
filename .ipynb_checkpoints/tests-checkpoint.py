a24770075768627d40dcfe47c0ca1177c985b17f
erikrecio <erikrecio@gmail.com>
2023-06-27 10:08:24 +0000

    plots

--- .ipynb_checkpoints/tests-checkpoint.py
+++ .ipynb_checkpoints/tests-checkpoint.py
@@ -5,23 +5,23 @@ from main import main
 
 
 # for num_cpus in range(10,251,10):
-for layers in range(9,11):
-    print(layers)
+# for layers in range(1,11):
+#     print(layers)
     
-    folder_name = "10. 1qubit, 2D, increasing layers"
-    dim_x = 2
-    n_qubits = 1 #layers*dim_x
+#     folder_name = "11. 1qubit, 3D, increasing layers"
+#     dim_x = 3
+#     n_qubits = 1 #layers*dim_x
 
-    circuit_class = Circuit_1qubit # Simple_circuit_marked, Circuit_with_weights, Circuit_n
-    circuit = circuit_class(n_qubits, dim_x, layers)
+#     circuit_class = Circuit_1qubit # Simple_circuit_marked, Circuit_with_weights, Circuit_n, Circuit_1qubit
+#     circuit = circuit_class(n_qubits, dim_x, layers)
 
-    weights_samples = 10000**(1/circuit.dim_w)
-    weights_search = "random"  # random, grid
-    bins_hist = 100
-    num_cpus = 160
+#     weights_samples = 10000**(1/circuit.dim_w)
+#     weights_search = "random"  # random, grid
+#     bins_hist = 100
+#     num_cpus = 160
 
-    dev = qml.device('lightning.qubit', wires=circuit.n_qubits)
-    main(weights_samples, weights_search, bins_hist, circuit, dev, folder_name, num_cpus)
+#     dev = qml.device('lightning.qubit', wires=circuit.n_qubits)
+#     main(weights_samples, weights_search, bins_hist, circuit, dev, folder_name, num_cpus)
 
 
 #%%
@@ -47,29 +47,28 @@ for layers in range(9,11):
 # print(a[1]/b[4])
 
 # #%%
-# import pennylane as qml
-# from circuits import *
-# from main import main
+import pennylane as qml
+from circuits import *
+from main import main
 
 
-# for dim_x in range(2,11):
-#     print(dim_x)
+for dim_x in range(7,11):
+    print(dim_x)
     
-#     folder_name = "8. Layers = 2, increasing dim_x and n"
-#     layers = 2 #n_qubits//dim_x
-#     n_qubits = layers*dim_x
-#     multicpu = False
+    folder_name = "07. Layers = 1, increasing dim_x and n"
+    layers = 1 #n_qubits//dim_x
+    n_qubits = layers*dim_x
 
-#     circuit_class = Circuit_3 # Simple_circuit_marked, Circuit_with_weights, Circuit_n
-#     circuit = circuit_class(n_qubits, dim_x, layers)
+    circuit_class = Circuit_3 # Simple_circuit_marked, Circuit_with_weights, Circuit_n
+    circuit = circuit_class(n_qubits, dim_x, layers)
 
-#     weights_samples = 10000**(1/circuit.dim_w)
-#     weights_search = "random"  # random, grid
-#     bins_hist = 100
+    weights_samples = 10000**(1/circuit.dim_w)
+    weights_search = "random"  # random, grid
+    bins_hist = 100
+    num_cpus = 160
 
-
-#     dev = qml.device('lightning.qubit', wires=circuit.n_qubits)
-#     main(weights_samples, weights_search, bins_hist, circuit, dev, folder_name, multicpu)
+    dev = qml.device('lightning.qubit', wires=circuit.n_qubits)
+    main(weights_samples, weights_search, bins_hist, circuit, dev, folder_name, num_cpus)
 
 # #%%
 # import time
