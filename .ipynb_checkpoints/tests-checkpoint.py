dba5fd1342b988355447f3d594847bdfb966438c
erikrecio <erikrecio@gmail.com>
2023-07-03 09:47:41 +0000

    plots

--- .ipynb_checkpoints/tests-checkpoint.py
+++ .ipynb_checkpoints/tests-checkpoint.py
@@ -3,41 +3,18 @@ import pennylane as qml
 from circuits import *
 from main import main
 
-# Experiment 06
+# Experiment 16
 
-# for dim_x in range(5,11):
-#     print(dim_x)
+for layers_x in range(2,11):
+    print(layers_x)
 
-#     folder_name = f"06. Circuit 3, 10qubits, max L, increasing dim_x"
-#     # dim_x = 2
-#     n_qubits = 10 #layers_x*dim_x
-#     layers_x = n_qubits//dim_x
-#     layers_p = 1
-    
-#     circuit_class = Circuit_3 # Simple_circuit_marked, Circuit_with_weights, Circuit_n, Circuit_1qubit, Circuit_big
-    
-#     circuit = circuit_class(n_qubits, dim_x, layers_x, layers_p)
-#     weights_samples = 10000**(1/circuit.dim_w)
-#     weights_search = "random"  # random, grid
-#     bins_hist = 100
-#     num_cpus = 160
-
-#     dev = qml.device('lightning.qubit', wires=circuit.n_qubits)
-#     main(weights_samples, weights_search, bins_hist, circuit, dev, folder_name, num_cpus)
-    
-
-# Experiment 07
-
-for dim_x in range(2,11):
-    print(dim_x)
-
-    folder_name = f"07. Layers = 1, increasing dim_x and n"
-    # dim_x = 2
-    layers_x = 1
+    folder_name = f"16. 1D, Lp=1, increasing Lx and n"
+    dim_x = 1
+    # layers_x = 2
     layers_p = 1
     n_qubits = layers_x*dim_x
-    circuit_class = Circuit_3 # Simple_circuit_marked, Circuit_with_weights, Circuit_n, Circuit_1qubit, Circuit_big
-    
+    circuit_class = Circuit_big # Simple_circuit_marked, Circuit_with_weights, Circuit_n, Circuit_1qubit, Circuit_big
+
     circuit = circuit_class(n_qubits, dim_x, layers_x, layers_p)
     weights_samples = 10000**(1/circuit.dim_w)
     weights_search = "random"  # random, grid
@@ -48,18 +25,18 @@ for dim_x in range(2,11):
     main(weights_samples, weights_search, bins_hist, circuit, dev, folder_name, num_cpus)
     
 
-# Experiment 08
+# Experiment 17
 
-for dim_x in range(1,11):
-    print(dim_x)
+for layers_x in range(2,11):
+    print(layers_x)
 
-    folder_name = f"08. Layers = 2, increasing dim_x and n"
-    # dim_x = 2
-    layers_x = 2
-    layers_p = 1
+    folder_name = f"17. 1D, Lp=5, increasing Lx and n"
+    dim_x = 1
+    # layers_x = 2
+    layers_p = 5
     n_qubits = layers_x*dim_x
-    circuit_class = Circuit_3 # Simple_circuit_marked, Circuit_with_weights, Circuit_n, Circuit_1qubit, Circuit_big
-    
+    circuit_class = Circuit_big # Simple_circuit_marked, Circuit_with_weights, Circuit_n, Circuit_1qubit, Circuit_big
+
     circuit = circuit_class(n_qubits, dim_x, layers_x, layers_p)
     weights_samples = 10000**(1/circuit.dim_w)
     weights_search = "random"  # random, grid
