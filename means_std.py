e7dbf3a53fa7d7e52ff1d61ba70d0f1698bc1631
Erik Recio <erik.recio@gmail.com>
2023-08-29 07:47:01 +0200

    commit

--- means_std.py
+++ means_std.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # def means_std():
-mypath = "Data/3. Circuit 1 1D 100.000/"
+mypath = "Data/22. PLOT 3 - Lx=1, Lp=5, increasing dim_x and n/"
 
 csv_names = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
@@ -27,13 +27,49 @@ for csv_name in csv_names:
     # print(csv.std()[0:3])
 
 
-plt.scatter(range(2,15), mean_inf)
+plt.scatter(range(2,11), mean_inf)
 plt.show
 
 #%%
-plt.scatter(range(2,15), mean_rkhs)
+from scipy.optimize import curve_fit
+import numpy as np
+
+x = range(2,11)
+y = mean_rkhs
+popt, pcov = curve_fit(lambda t, a, b, c: a * np.exp(b * t) + c, x, y)
+
+a = popt[0]
+b = popt[1]
+c = popt[2]
+
+x_fitted = np.linspace(np.min(x), np.max(x), 100)
+y_fitted = a * np.exp(b * x_fitted) + c
+
+plt.scatter(x, y, label = "Raw data")
+plt.plot(x_fitted, y_fitted, 'k', label=f'Fitted curve: {round(a,2)}exp({round(b,2)}x){round(c,2)}')
+plt.legend()
 plt.show
 
 #%%
-plt.scatter(range(2,15), mean_inf_rkhs)
+plt.scatter(range(2,11), mean_inf_rkhs)
+plt.show
+
+# %%
+from scipy.optimize import curve_fit
+import numpy as np
+
+x = range(2,11)
+y = mean_rkhs
+popt, pcov = curve_fit(lambda t, a, b, c: a*t*t + b*t + c, x, y)
+
+a = popt[0]
+b = popt[1]
+c = popt[2]
+
+x_fitted = np.linspace(np.min(x), np.max(x), 100)
+y_fitted = a*x_fitted**2 + b*x_fitted + c
+
+plt.scatter(x, y, label = "Raw data")
+plt.plot(x_fitted, y_fitted, 'k', label=f'Fitted curve: {round(a,2)}x**2 + {round(b,2)}x + {round(c,2)}')
+plt.legend()
 plt.show
